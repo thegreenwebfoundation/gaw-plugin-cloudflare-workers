7ae4f0f03fdbd438b2fde358111a2de0d07b1540
fershad <27988517+fershad@users.noreply.github.com>
2025-05-09 14:41:30 +0800

    Update deps

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,15 @@
-import { KVNamespace, Response as CFResponse } from "@cloudflare/workers-types";
+import {
+  KVNamespace,
+  Response as CFResponse,
+  ExecutionContext,
+  Request as CFRequest,
+} from "@cloudflare/workers-types";
 
 export type locationOptions = {
   mode?: "country" | "latlon";
 };
 
-export type cloudflareRequest = {
-  cf?: {
-    country?: string;
-    latitude?: number;
-    longitude?: number;
-  };
-};
+export type cloudflareRequest = CFRequest;
 
 export type locationResponse = {
   country?: string;
@@ -36,3 +35,5 @@ export type cloudflareResponse = {
 export type kvOptions = {
   expirationTtl?: number; // in seconds
 };
+
+export type cloudflareContext = ExecutionContext;
